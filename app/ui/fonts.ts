4ad3c8aad0aecbdfe50bcdf5f7f10c9bc64c6326
ANmishra-a <anujm2266@gmail.com>
2024-12-21 12:45:37 +0530

    error in deploying fixed added subset for font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,8 @@ import { Inter } from "next/font/google";
 import { Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
-export const lusitana = Lusitana({ weight: "400" });
+export const lusitana = Lusitana({
+  subsets: ["latin"], // Specify the 'latin' subset
+  weight: ["400", "700"], // Optional, specify the font weights you need
+  preload: true, // Enable preloading if you want to preload the font
+});
